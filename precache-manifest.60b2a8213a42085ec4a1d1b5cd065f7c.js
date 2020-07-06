f3c5b899e145eb0534a6498d67f228d6b916d700
Bryan White <bryanchriswhite@gmail.com>
2020-07-06 14:17:21 +0200

    build

---
+++ precache-manifest.60b2a8213a42085ec4a1d1b5cd065f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58af1bbbbfcde8854418343abcebc3e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6a3098e6faa808815ac",
+    "url": "/static/css/main.f508cee8.chunk.css"
+  },
+  {
+    "revision": "7888edbadf8434df9b49",
+    "url": "/static/js/2.95f98af2.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/static/js/2.95f98af2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a3098e6faa808815ac",
+    "url": "/static/js/main.79a32ebf.chunk.js"
+  },
+  {
+    "revision": "0fab776da41d98c64114",
+    "url": "/static/js/runtime-main.38043156.js"
+  }
+]);
